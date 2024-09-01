c568ea414e8a38e7ece1c9d9387e6770df891d7a
1underman123 <gjw4492@gmail.com>
2024-08-31 20:20:42 -0400

    cool i guess

--- configs/nvim/lua/plugins.lua
+++ configs/nvim/lua/plugins.lua
@@ -1,1 +1,85 @@
-/nix/store/21vpgxnyrj964v5dx90f8g75bvqb5bvl-home-manager-files/.config/nvim/lua/plugins.lua
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+
+require("lazy").setup({
+  {
+    'akinsho/bufferline.nvim',
+    version = "*",
+    dependencies = 'nvim-tree/nvim-web-devicons'
+  },
+  {
+    "nvim-neo-tree/neo-tree.nvim",
+    branch = "v3.x",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+      "MunifTanjim/nui.nvim",
+      -- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
+    }
+  },
+  {
+    'AlphaTechnolog/pywal.nvim',
+  },
+  	-- Vscode-like pictograms
+  {
+    'windwp/nvim-autopairs',
+    event = "InsertEnter",
+    config = true
+    -- use opts = {} for passing setup options
+    -- this is equalent to setup({}) function
+  },
+	{
+		"onsails/lspkind.nvim",
+		event = { "VimEnter" },
+	},
+	-- Auto-completion engine
+	{
+		"hrsh7th/nvim-cmp",
+		dependencies = {
+			"lspkind.nvim",
+			"hrsh7th/cmp-nvim-lsp", -- lsp auto-completion
+			"hrsh7th/cmp-buffer", -- buffer auto-completion
+			"hrsh7th/cmp-path", -- path auto-completion
+			"hrsh7th/cmp-cmdline", -- cmdline auto-completion
+		},
+		config = function()
+			require("config.nvim-cmp")
+		end,
+	},
+	-- Code snippet engine
+	{
+		"L3MON4D3/LuaSnip",
+		version = "v2.*",
+	},
+  {
+    'mrcjkb/rustaceanvim',
+    version = '^5', -- Recommended
+  lazy = false, -- This plugin is already lazy
+  },
+  -- LSP manager
+  'williamboman/mason.nvim',
+  'williamboman/mason-lspconfig.nvim',
+  'neovim/nvim-lspconfig',
+  -- code snippets
+  'rafamadriz/friendly-snippets',
+  {
+	  "L3MON4D3/LuaSnip",
+	  -- follow latest release.
+	  version = "v2.*", -- Replace <CurrentMajor> by the latest released major (first number of latest release)
+	  -- install jsregexp (optional!).
+	  build = "make install_jsregexp"
+  },
+})
+
+
+require("lazy").setup({})
